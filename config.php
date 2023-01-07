94e05eb308d9fc427cac4e885b8247b27dbd942a
Ubuntu <ubuntu@ip-10-0-0-209.ap-southeast-1.compute.internal>
2023-01-07 03:23:29 +0000

    sosialmedia

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "devopscilsy";
-$db_pass = "1234567890";
+$db_host = "dbsosmed";
+$db_user = "root";
+$db_pass = "test";
 $db_name = "dbsosmed";
 
 try {    
